9fe808937efde8d64e6367f8495f55b0db0396e5
cristobalmunoz <cristobal.vildosola@gmail.com>
2018-02-01 12:13:32 -0300

    update plugin jar location

--- run/plugin-screen.sh
+++ run/plugin-screen.sh
@@ -10,7 +10,7 @@ echo "Starting the plugin in screen named plugin."
 echo "Check you are connected to the ESO VPN otherwise the process will end."
 
 ## run in a detached screen
-screen -S plugin -dm java -jar build/libs/AlmaWeatherStationPlugin.jar
+screen -S plugin -dm java -jar dist/AlmaWeatherStationPlugin.jar
 
 ## return to the original folder, allowing the user to instantly run something else
 cd ../../integration-tools/run/
