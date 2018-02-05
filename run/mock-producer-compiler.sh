67e7a8f2c307f55d008066c3940d572e69a75da3
Sebastian <sebafehlandt@gmail.com>
2018-02-05 17:47:57 -0300

    Refactor scripts to run mock-producer and its demo in order to use screen

--- run/mock-producer-compiler.sh
+++ run/mock-producer-compiler.sh
@@ -5,5 +5,3 @@ cd ../../ias/WebServerSender/test
 ant build
 cd ../..
 ant build
-cd ./WebServerSender/test/java
-iasRun.py -l j org.eso.ias.webserversender.MockCoreKafkaProducer
--- run/sample-demo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo 'Starting demo, this may take a few moments, please wait (Ctrl+C to cancel)'
-echo '---------------------------------------------------------'
-cd ..
-cd run
-pwd
-parallel ::: './run-kafka.sh' './webserver-screen.sh' './display-screen.sh' './mock-producer.sh' './sender-screen.sh'
