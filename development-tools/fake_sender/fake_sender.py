87bcf7f1446e25d068133fa8909cab32e85ec3b1
Sebastian <sebafehlandt@gmail.com>
2019-04-01 17:51:00 -0300

    Make basic script to send messages

--- development-tools/fake_sender/fake_sender.py
+++ development-tools/fake_sender/fake_sender.py
@@ -1,9 +1,12 @@
 import os
 import sys
 import tornado
+from clients import WSClient
 
 DEFAULT_HOST = 'localhost:8000'
-DEFAULT_PASS = ''
+DEFAULT_PASS = 'dev_pass'
+DEFAULT_SEND_RATE = 1000
+DEFAULT_RECONNECTION_RATE = 1000  # One second to evaluate reconnection
 
 
 def get_websocket_url(kwargs):
@@ -23,13 +26,51 @@ def get_websocket_url(kwargs):
     if kwargs['password'] is not None:
         password = kwargs['password']
 
-    return 'ws://{}/stream/?password={}'.format(host, password)
+    if password and password != '':
+        return 'ws://{}/stream/?password={}'.format(host, password)
+    else:
+        return 'ws://{}/stream/'.format(host)
 
 
 def main(**kwargs):
-    print('kwargs: ', kwargs)
     url = get_websocket_url(kwargs)
-    print('url: ', url)
+    print('Going to send messages to: ', url)
+
+    ws_client = WSClient(url, kwargs)
+
+    def send_alarm():
+        if ws_client.is_connected():
+            msg = {
+                'stream': 'broadcast',
+                'payload': {
+                    'action': 'list'
+                }
+            }
+            print('Sending message: ', msg)
+            ws_client.send_message(msg)
+
+    send_rate = DEFAULT_SEND_RATE
+    reconnection_rate = DEFAULT_RECONNECTION_RATE
+
+    def ws_reconnection():
+        if not ws_client.is_connected():
+            ws_client.reconnect()
+
+    broadcast_task = tornado.ioloop.PeriodicCallback(
+        send_alarm,
+        send_rate
+    )
+
+    reconnection_task = tornado.ioloop.PeriodicCallback(
+        ws_reconnection,
+        reconnection_rate
+    )
+    tasks = [broadcast_task, reconnection_task]
+
+    for task in tasks:
+        task.start()
+
+    tornado.ioloop.IOLoop.current().start()
 
 
 if __name__ == '__main__':
@@ -38,4 +79,4 @@ if __name__ == '__main__':
     else:
         host = DEFAULT_HOST
     password = os.getenv('WEBSOCKET_PASS', DEFAULT_PASS)
-    main(host=host, password=password)
+    main(host=host, password=password, verbosity=1)
